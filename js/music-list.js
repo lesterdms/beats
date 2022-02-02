be285f5c37a4a990b16e477799d408169e240b69
lesterdms <69951564+lesterdms@users.noreply.github.com>
2022-02-02 16:32:33 +0800

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,62 @@
+let allMusic = [
+  {
+    name: "Simon Servida - Company",
+    artist: "Lester Salazar (Remix)",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Dreams",
+    artist: "Lester Salazar",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Fall",
+    artist: "Lester Salazar",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "No Sleep",
+    artist: "Lester Salazar",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Wool Gloves",
+    artist: "imagiro",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Single Phial",
+    artist: "HM Surf",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+    name: "Alone Time",
+    artist: "Purple Cat",
+    img: "music-7",
+    src: "music-7"
+  },
+  {
+    name: "Fever",
+    artist: "psalm trees",
+    img: "music-8",
+    src: "music-8"
+  },
+  {
+    name: "bumps",
+    artist: "Barradeen",
+    img: "music-9",
+    src: "music-9"
+  },
+  {
+    name: "Circle",
+    artist: "H.1",
+    img: "music-10",
+    src: "music-10"
+  },
+];
